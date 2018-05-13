a1b3dd9d29cc3862973697e52ebeab6955fc21bf
Bram Swennen <bramswennen1@gmail.com>
2018-05-13 13:52:48 +0200

    fixed updateAll

--- updateAll.py
+++ updateAll.py
@@ -1,8 +1,8 @@
 import src.data.Market as Market
 
-markets = {'XAMS', 'XBUR', 'XPAR'}
+markets = {'XAMS', 'XBRU', 'XPAR'}
 
 for m in markets:
-    market = Market(m)
+    market = Market.Market(m)
     market.download_all_stock_data()
     market.momenta()
